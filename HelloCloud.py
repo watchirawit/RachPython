cb204e3327496102b3e2c73e22ba476d5e2303c7
watchirawit <68271994+watchirawit@users.noreply.github.com>
2021-08-11 10:38:54 +0700

    Ready

--- HelloCloud.py
+++ HelloCloud.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello RUK-COM!</h1>'
+    return '<h1>Hello Astre NO.1 in Thailand by zabeen!</h1>'
 
 
 @app.route('/home',methods=['GET','POST'])
